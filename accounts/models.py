3dd33088b49b04bef0c22cc0a288428884f996d3
Mustafa Haita <mustafahaitaa@gmail.com>
2023-08-13 16:53:20 +0300

    vendor profile form validation

--- accounts/models.py
+++ accounts/models.py
@@ -91,8 +91,7 @@ class UserProfile(models.Model):
         upload_to='users/profile_pictures', blank=True, null=True)
     cover_photo = models.ImageField(
         upload_to='users/cover_photos', blank=True, null=True)
-    adress_line_1 = models.CharField(max_length=50, blank=True, null=True)
-    adress_line_2 = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
@@ -101,9 +100,6 @@ class UserProfile(models.Model):
     longtitude = models.CharField(max_length=20, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-
-    def full_address(self):
-        return f'{self.adress_line_1}, {self.adress_line_2}'
 
     def __str__(self):
         return self.user.email
