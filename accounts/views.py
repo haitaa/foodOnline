b627b82e7e06ea319327d25b118190847e476726
Mustafa Haita <mustafahaitaa@gmail.com>
2023-08-17 17:17:15 +0300

    marketplace implementation

--- accounts/views.py
+++ accounts/views.py
@@ -12,6 +12,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
+from django.template.defaultfilters import slugify
 
 # Restrict the vendor from accessing the customer page
 
@@ -35,7 +36,7 @@ def check_role_customer(user):
 def registerUser(request):
     if request.user.is_authenticated:
         messages.warning(request, 'You are already logged in.')
-        return redirect('customerDashboard')
+        return redirect('myAccount')
     elif request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
@@ -101,6 +102,8 @@ def registerVendor(request):
             user.save()
             vendor = vendor_form.save(commit=False)
             vendor.user = user
+            vendor_name = vendor_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
